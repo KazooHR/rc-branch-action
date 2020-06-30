93209ab2b64a18e2cd47dc4bdd07ff0cf77aa38a
Jeff Israel <stripethree@users.noreply.github.com>
2020-06-30 17:35:28 -0500

    Main functionality to create tags via API calls (#2)
    - Basic checks on the provided environment varibles
    - Adds query to call the GitHub GraphQL API to get ref information on the provided repository and branch
    - Adds mutation to create a new ref (tag) in the provided repository
    - Handles cases where no prior tags exists, creating a tag named <BRANCH_NAME>-001
    - Handles subsequent cases to create tags with incrementing build number suffixes
    - Provides output, including error states, in the format that GitHub Actions processes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,95 @@
-console.log("rc-branch-action");
+const core = require("@actions/core");
+const { graphql } = require("@octokit/graphql");
+
+const { getBaseTagNode, zeroPad } = require("./helpers");
+const { CREATE_TAG_MUTATION, GET_REFS_QUERY } = require("./queries");
+
+// Needed as an identifer for creating references via the GraphQL API
+// https://developer.github.com/v4/mutation/createref/
+const CLIENT_ID = `kazoohr/rc-branch-action`;
+
+async function createNewTag(token, tagName, commitOid, repositoryId) {
+  return graphql(CREATE_TAG_MUTATION, {
+    clientId: CLIENT_ID,
+    refName: `refs/tags/${tagName}`,
+    commitOid,
+    repositoryId,
+    headers: {
+      authorization: `token ${token}`,
+    },
+  });
+}
+
+async function getBranchRefs(token, owner, repoName, queryStr) {
+  return graphql(GET_REFS_QUERY, {
+    owner,
+    repoName,
+    queryStr,
+    headers: {
+      authorization: `token ${token}`,
+    },
+  });
+}
+
+if (!process.env.GITHUB_REF) {
+  console.error("Missing GITHUB_REPOSITORY");
+  return;
+}
+
+if (!process.env.GITHUB_REPOSITORY) {
+  console.error("Missing GITHUB_REPOSITORY");
+  return;
+}
+
+if (!process.env.GITHUB_TOKEN) {
+  console.error("Missing GITHUB_TOKEN");
+  return;
+}
+
+const branchName = process.env.GITHUB_REF.split("/").pop();
+const [owner, repoName] = process.env.GITHUB_REPOSITORY.split("/");
+const token = process.env.GITHUB_TOKEN;
+
+if (!owner || !repoName) {
+  console.error("Invalid GITHUB_REPOSITORY value");
+  return;
+}
+
+getBranchRefs(token, owner, repoName, branchName)
+  .then((data) => {
+    const repositoryId = data.repository.id;
+    const tagNodes = data.repository.refs.nodes.filter((node) =>
+      node.name.startsWith("tags")
+    );
+    const lastTagNode = tagNodes.pop() || getBaseTagNode(branchName);
+
+    const branchNodes = data.repository.refs.nodes.filter(
+      (node) => node.name === `heads/${branchName}`
+    );
+    const branchNode = branchNodes.pop();
+    if (!branchNode) {
+      throw new Error("No matching branch name found in graphql response.");
+    }
+
+    const lastTag = lastTagNode.name.split("/").pop();
+    const lastPatch = parseInt(lastTag.split("-").pop());
+    const newTag = branchName.concat("-").concat(zeroPad(lastPatch + 1, 3));
+
+    const branchOid = branchNode.target.oid;
+    const lastTagOid = lastTagNode.target.oid;
+
+    if (lastTagOid !== null && branchOid === lastTagOid) {
+      throw new Error(
+        "No new branch commits. Cowardly refusing to create a new tag."
+      );
+    }
+    return createNewTag(token, newTag, branchOid, repositoryId);
+  })
+  .then((data) => {
+    const newTag = data.createRef.ref.name;
+    const newRefId = data.createRef.ref.id;
+    core.setOutput(`Tag ${newTag} created with ref id ${newRefId}.`);
+  })
+  .catch((errorMessage) => {
+    core.setFailed(errorMessage);
+  });
---
+++ src/queries.js
@@ -0,0 +1,31 @@
+CREATE_TAG_MUTATION = `
+  mutation($clientId: String!, $refName: String!, $commitOid: String!, $repositoryId: String! ) {
+    createRef( input:{ clientMutationId: $clientId, name: $refName, oid: $commitOid, repositoryId: $repositoryId } ) {
+      clientMutationId
+      ref {
+        id
+        name
+        prefix
+      }
+    }
+  }
+`;
+
+GET_REFS_QUERY = `
+  query($owner: String!, $repoName: String!, $queryStr: String!) {
+    repository(name: $repoName owner: $owner) {
+      id
+      refs(refPrefix: "refs/" first: 100 query: $queryStr orderBy: { field: TAG_COMMIT_DATE, direction: ASC } ) {
+        nodes {
+          name
+          target {
+            oid
+          }
+        }
+      }
+    }
+  }
+`;
+
+exports.CREATE_TAG_MUTATION = CREATE_TAG_MUTATION;
+exports.GET_REFS_QUERY = GET_REFS_QUERY;
